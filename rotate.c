9a8b19268dbe63de1500f5a9f4b63ac422a24a0b
joao-per <rodriguespereirajoaopaulo@gmail.com>
2023-02-15 19:11:21 +0000

    simplified code

--- rotate.c
+++ rotate.c
@@ -35,3 +35,36 @@ void	do_rotate(t_stack **stack_a, t_stack **stack_b, char *op)
 		ft_putstr("rr\n");
 	}
 }
+
+static void rrotate(t_stack **stack)
+{
+	t_stack *head = *stack;
+	t_stack *before_tail = get_penultimo_value(*stack);
+	t_stack *tail = get_bottom_value(*stack);
+
+	if (*stack == NULL || (*stack)->next == NULL)
+		return;
+	before_tail->next = NULL;
+	tail->next = head;
+	*stack = tail;
+}
+
+void	do_rrotate(t_stack **stack_a, t_stack **stack_b, char *op)
+{
+	if (ft_strcmp(op, "rra") == 0)
+	{
+		rrotate(stack_a);	
+		ft_putstr("rra\n");
+	}
+	else if (ft_strcmp(op, "rrb") == 0)
+	{
+		rrotate(stack_b);
+		ft_putstr("rrb\n");
+	}
+	else if (ft_strcmp(op, "rrr") == 0)
+	{
+		rrotate(stack_a);
+		rrotate(stack_b);
+		ft_putstr("rrr\n");
+	}
+}

1559d103512c4a85d1d40a93b0060efbdff135d0
joao-per <rodriguespereirajoaopaulo@gmail.com>
2023-02-15 19:13:01 +0000

    simplified code

--- pushandswap.c
+++ pushandswap.c
@@ -1,4 +1,29 @@
 #include "push_swap.h"
+void	push(t_stack **src, t_stack **dest)
+{
+	t_stack	*temp;
+
+	temp = src;
+	if (*src == NULL)
+		return ;
+	*src = (*src)->next;
+	temp->next = *dest;
+	*dest = temp;
+}
+
+void	do_push(t_stack **stack_a, t_stack **stack_b, char *op)
+{
+	if (ft_strcmp(op, "pa") == 0)
+	{
+		push(stack_b, stack_a);
+		ft_putstr("pa\n");
+	}
+	if (ft_strcmp(op, "pb") == 0)
+	{
+		push(stack_a, stack_b);
+		ft_putstr("pb\n");
+	}
+}
 
 void	swap(t_stack *stack)
 {
